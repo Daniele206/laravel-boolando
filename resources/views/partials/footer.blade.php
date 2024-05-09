f69c679c3b3745223abe6fe35a567eff1739e829
Daniele206 <danieledeieso2004@gmail.com>
2024-05-09 16:35:10 +0200

    [add] dinamic footer contet

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,24 +1,24 @@
+@php
+
+$infos = config('menues.footerMenues.info');
+$socials = config('menues.footerMenues.socials');
+
+@endphp
+
 <footer>
     <div class="my_container">
         <div>
             <span class="agency">Boolando s.r.l.</span>
             <div>
             <ul class="my_menu">
-                <li>
-                    <a
-                    href="#"
-                    >text</a>
-                </li>
-                <li>
-                    <a
-                    href="#"
-                    >text</a>
-                </li>
-                <li>
-                    <a
-                    href="#"
-                    >text</a>
-                </li>
+                @foreach ($infos as $info)
+                    <li>
+                        <a
+                        class="{{ $info['text'] === 'Informativa sulla privacy' ? 'mx-2' : ''}}"
+                        href="#"
+                        >{{$info['text']}}</a>
+                    </li>
+                @endforeach
             </ul>
             </div>
         </div>
@@ -26,31 +26,13 @@
             <span class="social_direct">Trovaci anche su</span>
             <div>
             <ul class="my_menu justify-content-between">
-                <li>
-                    <a href="#">
-                        <i>ico</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i>ico</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i>ico</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i>ico</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i>ico</i>
-                    </a>
-                </li>
+                @foreach ($socials as $social)
+                    <li>
+                        <a href="#">
+                            <i class="{{$social['class']}}"></i>
+                        </a>
+                    </li>
+                @endforeach
             </ul>
             </div>
         </div>
