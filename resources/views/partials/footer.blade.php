0d362b6775d6927978b1ae077361dd62a2bcc5b7
Daniele206 <danieledeieso2004@gmail.com>
2024-05-09 15:11:14 +0200

    [add] static header/footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,58 @@
+<footer>
+    <div class="my_container">
+        <div>
+            <span class="agency">Boolando s.r.l.</span>
+            <div>
+            <ul class="my_menu">
+                <li>
+                    <a
+                    href="#"
+                    >text</a>
+                </li>
+                <li>
+                    <a
+                    href="#"
+                    >text</a>
+                </li>
+                <li>
+                    <a
+                    href="#"
+                    >text</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        <div>
+            <span class="social_direct">Trovaci anche su</span>
+            <div>
+            <ul class="my_menu justify-content-between">
+                <li>
+                    <a href="#">
+                        <i>ico</i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i>ico</i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i>ico</i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i>ico</i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i>ico</i>
+                    </a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,41 @@
+<header>
+    <div class="my_container">
+        <div class="my_section">
+            <ul class="my_menu">
+                <li>
+                    <a
+                    href="#"
+                    >text</a>
+                </li>
+                <li>
+                    <a
+                    href="#"
+                    >text</a>
+                </li>
+                <li>
+                    <a
+                    href="#"
+                    >text</a>
+                </li>
+            </ul>
+        </div>
+        <div class="my_section d-flex justify-content-center">
+            <img class="w-75" src="/img/boolean-logo.png" alt="Boolean">
+        </div>
+        <div class="my_section d-flex justify-content-end">
+            <ul class="my_menu">
+                <li>
+                    <a href="#">
+                    <i>ico</i>
+                    </a>
+                    <a href="#">
+                    <i>ico</i>
+                    </a>
+                    <a href="#">
+                    <i>ico</i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
