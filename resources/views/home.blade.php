048df41fd7cea8f87f26e008b2d46315514afa37
Daniele206 <danieledeieso2004@gmail.com>
2024-05-09 15:40:21 +0200

    [add] home to main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,40 @@
 @extends('layout.main')
 
-
+@section('contet')
+<div class="my_container">
+    <div class="my_card">
+        <div class="my_card_img">
+            <img class="my_img_first" src="/img/1.webp" alt="img1">
+            <div class="like">
+                <i class="fa-solid fa-heart"></i>
+            </div>
+            <div class="product_info">
+                <span>valore badge</span>
+            </div>
+            </div>
+            <div class="description">
+            <span>brand</span>
+            <span class="product-name">name</span>
+            <span>price &euro;</span>
+        </div>
+    </div>
+</div>
+<div class="my_container">
+    <div class="my_card">
+        <div class="my_card_img">
+            <img class="my_img_first" src="/img/1.webp" alt="img1">
+            <div class="like">
+                <i class="fa-solid fa-heart"></i>
+            </div>
+            <div class="product_info">
+                <span>valore badge</span>
+            </div>
+            </div>
+            <div class="description">
+            <span>brand</span>
+            <span class="product-name">name</span>
+            <span>price &euro;</span>
+        </div>
+    </div>
+</div>
+@endsection
